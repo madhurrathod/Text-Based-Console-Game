2d74675d432a0581919f67a319817421216def0e
Madhur Rathod <madhur.thanku@gmail.com>
2025-04-19 00:39:10 +0530

    press instruction

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App(){
 
   return(<>
       <PlayMusic />
-      <h4 style={{marginLeft: "3vw",width:"200px", background: "rgba(127, 233, 212, 0.65)", borderRadius: "50px", textAlign: "center", padding: "10px"}}>Open Console Window</h4>
+      <h4 style={{marginLeft: "3vw",width:"200px", background: "rgba(127, 233, 212, 0.65)", borderRadius: "50px", textAlign: "center", padding: "10px"}}>Open Console Window<br>Press cmd+opn+i</br></h4>
   </>)
 }
 export default App;
